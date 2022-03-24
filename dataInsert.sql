c3b9e9ed0055ede58fc05f0a51dcc1db78bd33a1
svetlanaTsurankova <101640274+svetlanaTsurankova@users.noreply.github.com>
2022-03-24 23:09:04 +0300

    task-10
    file create DB
    file dateInsert
    and SQl query

---
+++ dataInsert.sql
@@ -0,0 +1,46 @@
+/* laptop*/
+INSERT into test.laptop (code,model,speed,ram,hd,screen,price) values (30,'321',3600,32000,1024,15,7030);
+INSERT into test.laptop (code,model,speed,ram,hd,screen,price) values (31,'322',2200,16000,500,15,6003);
+INSERT into test.laptop (code,model,speed,ram,hd,screen,price) values (32,'323',3300,16000,250,14,4030);
+INSERT into test.laptop (code,model,speed,ram,hd,screen,price) values (33,'324',3120,12000,500,12,3050);
+INSERT into test.laptop (code,model,speed,ram,hd,screen,price) values (34,'325',2100,8000,120,10,1900);
+/*pc*/
+INSERT into test.pc (code,model,speed,ram,hd,cd,price) values (20,'123',3660,32000,1024,'4x',4030);
+INSERT into test.pc (code,model,speed,ram,hd,cd,price) values (21,'124',2100,4000,500,'4x',400);
+INSERT into test.pc (code,model,speed,ram,hd,cd,price) values (22,'125',3200,16000,250,'',700);
+INSERT into test.pc (code,model,speed,ram,hd,cd,price) values (23,'126',3120,12000,500,'2x',2050);
+INSERT into test.pc (code,model,speed,ram,hd,cd,price) values (24,'127',2440,8000,120,'',450);
+INSERT into test.pc (code,model,speed,ram,hd,cd,price) values (25,'128',2440,8000,500,'24x',1800);
+INSERT into test.pc (code,model,speed,ram,hd,cd,price) values (26,'129',2440,8000,500,'4x',1900);
+INSERT into test.pc (code,model,speed,ram,hd,cd,price) values (27,'130',3660,32000,1024,'4x',2500);
+/*printer*/
+INSERT into test.printer (code,model,color,type,price) values (10,'231','y','laser',403);
+INSERT into test.printer (code,model,color,type,price) values (11,'232','','matrix',100);
+INSERT into test.printer (code,model,color,type,price) values (12,'233','','laser',203);
+INSERT into test.printer (code,model,color,type,price) values (13,'234','','jet',303);
+INSERT into test.printer (code,model,color,type,price) values (14,'235','y','jet',503);
+INSERT into test.printer (code,model,color,type,price) values (15,'236','y','laser',403);
+/* product pc*/
+INSERT into test.product (maker,model,type) values ('lenovo','123','PC');
+INSERT into test.product (maker,model,type) values ('asus','124','PC');
+INSERT into test.product (maker,model,type) values ('hp','125','PC');
+INSERT into test.product (maker,model,type) values ('dell','126','PC');
+INSERT into test.product (maker,model,type) values ('sony','127','PC');
+INSERT into test.product (maker,model,type) values ('sony','128','PC');
+INSERT into test.product (maker,model,type) values ('sony','128','PC');
+INSERT into test.product (maker,model,type) values ('toshiba','1130','PC');
+/* product laptop*/
+INSERT into test.product (maker,model,type) values ('lenovo','321','laptop');
+INSERT into test.product (maker,model,type) values ('asus','322','laptop');
+INSERT into test.product (maker,model,type) values ('hp','323','laptop');
+INSERT into test.product (maker,model,type) values ('dell','324','laptop');
+INSERT into test.product (maker,model,type) values ('sony','325','laptop');
+/*product printer*/
+INSERT into test.product (maker,model,type) values ('epson','231','printer');
+INSERT into test.product (maker,model,type) values ('toshiba','232','printer');
+INSERT into test.product (maker,model,type) values ('kyocera','233','printer');
+INSERT into test.product (maker,model,type) values ('xerox','234','printer');
+INSERT into test.product (maker,model,type) values ('canon','235','printer');
+INSERT into test.product (maker,model,type) values ('canon','236','printer');
+INSERT into test.product (maker,model,type) values ('dell','237','printer');
+INSERT into test.product (maker,model,type) values ('hp','238','printer');
